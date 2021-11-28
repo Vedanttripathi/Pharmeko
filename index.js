2f11982807ffb82207f2e689c8ec7b7800c0ecba
Vedanttripathi <sumitvedant112@gmail.com>
2021-11-28 21:53:08 +0530

    v 1.2

--- index.js
+++ index.js
@@ -13,4 +13,11 @@ app.use(stocksroute);
 
 const port = process.env.PORT || 5000;
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.js'));
+    })
+
 app.listen(port, () => console.log('Server Up! and sound on port ' + port));
